3697d57b721be126760e6a38704af13cc0558344
M. Rudianto <m45rud@gmail.com>
2016-06-25 03:47:34 +0700

    Improved Code

--- resource/ams.sql
+++ resource/ams.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2016 at 10:37 AM
+-- Generation Time: Jun 24, 2016 at 10:36 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.5.19
 
@@ -23,15 +23,88 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_disposisi`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_disposisi` (
+`id_disposisi` int(7) NOT NULL,
+  `id_surat` int(7) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `sifat` enum('Penting, Segera, Khusus') NOT NULL,
+  `batas_waktu` date NOT NULL,
+  `catatatn` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_instansi`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_instansi` (
+`id_instansi` tinyint(1) NOT NULL,
+  `nama_instansi` varchar(150) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `nama_kepsek` varchar(50) NOT NULL,
+  `nip_kepsek` varchar(25) NOT NULL,
+  `website` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_surat_keluar`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_surat_keluar` (
+`id_surat` int(7) NOT NULL,
+  `no_agenda` int(7) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(50) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_catat` date NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_surat_masuk`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_surat_masuk` (
 `id_surat` int(7) NOT NULL,
   `no_agenda` int(7) NOT NULL,
+  `no_surat` varchar(100) NOT NULL,
   `asal_surat` varchar(250) NOT NULL,
-  `isi_ringkas` text NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(50) NOT NULL,
+  `indeks` varchar(100) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_diterima` date NOT NULL,
+  `keterangan` varchar(250) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=206 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_surat_masuk`
+--
+
+INSERT INTO `tbl_surat_masuk` (`id_surat`, `no_agenda`, `no_surat`, `asal_surat`, `isi`, `kode`, `indeks`, `tgl_surat`, `tgl_diterima`, `keterangan`) VALUES
+(162, 2, '2', '2', '2', '2', '2', '0000-00-00', '2016-06-24', '2'),
+(166, 2, '2', '2', '2', '2', '2', '0000-00-00', '2016-06-24', '2'),
+(167, 2, '2', '2', '2', '2', '2', '0000-00-00', '2016-06-24', '2'),
+(168, 2, '2', '2', '2', '2', '2', '0000-00-00', '2016-06-24', '2'),
+(169, 2, '2', 'gfgfghf', 'hjghjg', 'rerrtert', 'fghfhf', '0000-00-00', '2016-06-24', 'hghjgjh'),
+(170, 2, '2', '2', '2', '2', '2', '0000-00-00', '2016-06-24', '2'),
+(190, 1, '1', '1', '1', '1', '1', '0000-00-00', '2016-06-24', '1'),
+(200, 2, '432/234/234/23423/2016', 'Nganjuk', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,', 'H', 'HK', '2016-01-01', '2016-06-24', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore '),
+(202, 56757, '7676', 'Nganjukkkkk', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,', 'H', 'YH', '2016-01-31', '2016-06-24', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,'),
+(205, 2, '6678', 'Nganjuk', 'GHJg', 'G', 'G', '2016-06-25', '2016-06-25', 'ghjgj');
 
 -- --------------------------------------------------------
 
@@ -41,11 +114,11 @@ CREATE TABLE IF NOT EXISTS `tbl_surat_masuk` (
 
 CREATE TABLE IF NOT EXISTS `tbl_user` (
 `id_user` tinyint(1) NOT NULL,
-  `username` varchar(30) DEFAULT NULL,
-  `password` varchar(35) DEFAULT NULL,
-  `nama` varchar(50) DEFAULT NULL,
-  `nip` varchar(25) DEFAULT NULL,
-  `admin` tinyint(1) DEFAULT NULL
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
@@ -55,11 +128,29 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
 INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `nama`, `nip`, `admin`) VALUES
 (6, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'M. Rudianto', '-', 1),
 (7, 'disposisi', '13bb8b589473803f26a02e338f949b8c', 'Petugas Disposisi', '-', 2),
-(8, 'imam', 'eaccb8ea6090a40a98aa28c071810371', 'Imam Al Husna', '-', 3);
+(8, 'imam', 'eaccb8ea6090a40a98aa28c071810371', 'Imam Al Husna', '-', 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_disposisi`
+--
+ALTER TABLE `tbl_disposisi`
+ ADD PRIMARY KEY (`id_disposisi`);
+
+--
+-- Indexes for table `tbl_instansi`
+--
+ALTER TABLE `tbl_instansi`
+ ADD PRIMARY KEY (`id_instansi`);
+
+--
+-- Indexes for table `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+ ADD PRIMARY KEY (`id_surat`);
 
 --
 -- Indexes for table `tbl_surat_masuk`
@@ -78,10 +169,25 @@ ALTER TABLE `tbl_user`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_disposisi`
+--
+ALTER TABLE `tbl_disposisi`
+MODIFY `id_disposisi` int(7) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tbl_instansi`
+--
+ALTER TABLE `tbl_instansi`
+MODIFY `id_instansi` tinyint(1) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+MODIFY `id_surat` int(7) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `tbl_surat_masuk`
 --
 ALTER TABLE `tbl_surat_masuk`
-MODIFY `id_surat` int(7) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id_surat` int(7) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=206;
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
