1ecf16e502b8d0d856c77b336e5e4de4900cae7a
m45rud <m45rud@gmail.com>
2016-07-20 11:44:03 +0700

    update

--- include/footer.php
+++ include/footer.php
@@ -11,12 +11,12 @@
 <footer class="page-footer">
     <div class="container">
            <div class="row">
-               <br/><br/>
+               <br/>
            </div>
     </div>
 <div class="footer-copyright blue-grey darken-1 white-text">
     <div class="container" id="footer">
-        <span class="white-text">&copy; <?php echo date("Y"); ?> Made with <i class="material-icons md-18">favorite</i> by <a class="white-text" href="http://masrud.com" target="_blank">M. Rudianto</a></span>
+        <span class="white-text">&copy; <?php echo date("Y"); ?> &nbsp;|&nbsp;  Made with <i class="material-icons md-18">favorite</i> by <a class="white-text" href="http://masrud.com" target="_blank">M. Rudianto</a></span>
         <div class="right hide-on-small-only">
             <?php
                 $query = mysqli_query($config, "SELECT * FROM tbl_instansi");
