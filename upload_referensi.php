e08e3346d0227b20b4c347c3c79f034cf35b2d7f
m45rud <m45rud@gmail.com>
2016-07-20 09:47:15 +0700

    Change text

--- upload_referensi.php
+++ upload_referensi.php
@@ -188,7 +188,7 @@
                                     </form>
                                 </p><br/>
 
-                                <p class="kata"><strong>CATATAN PENTING!</strong><br/>Secara default data yang ada akan diganti dengan data yang baru, jika tidak ingin menghapus data yang sudah ada, silakan centang checkbox <i class="material-icons">check_box_outline_blank</i> dibawah form file.</p>
+                                <p class="kata"><strong>PERINGATAN!</strong><br/>Secara default data yang ada akan diganti dengan data yang baru, jika tidak ingin menghapus data yang sudah ada, silakan centang checkbox <i class="material-icons">check_box_outline_blank</i> dibawah form file.</p>
                             </div>
                             <div class="card-action">
                                 <form method="post" enctype="multipart/form-data">
