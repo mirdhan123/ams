ab8cf2e07534d0a8eb427d6212864fa8bf838df4
m45rud <m45rud@gmail.com>
2016-07-22 06:48:11 +0700

    update css

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
         }
         .container {
             max-width: 100%;
-            margin-top: 2.2rem;
+            margin-top: 3.7rem;
         }
         #logo {
             display: block;
