157995a4ad50c5929a97919f4a2d20ef0c24bd34
m45rud <m45rud@gmail.com>
2016-07-27 14:31:15 +0700

    update

--- user.php
+++ user.php
@@ -21,17 +21,14 @@
             }
         } else {
 
-            //pagging
-            $limit = 5;
-            $pg = @$_GET['pg'];
-                if(empty($pg)){
-                    $curr = 0;
-                    $pg = 1;
-                } else {
-                    $curr = ($pg - 1) * $limit;
-                }
+            if($_SESSION['admin'] != 1 AND $_SESSION['admin'] != 3){
+                echo '<script language="javascript">
+                        window.alert("ERROR! Anda tidak memiliki hak akses untuk menambahkan data");
+                        window.location.href="./admin.php?page=ref";
+                      </script>';
+            } else {
 
-                $query = mysqli_query($config, "SELECT * FROM tbl_user LIMIT $curr, $limit");
+                $query = mysqli_query($config, "SELECT * FROM tbl_user");
                 echo '<!-- Row Start -->
                     <div class="row">
                         <!-- Secondary Nav START -->
@@ -150,47 +147,7 @@
 
                     </div>
                     <!-- Row form END -->';
-
-                    $query = mysqli_query($config, "SELECT * FROM tbl_user");
-                    $cdata = mysqli_num_rows($query);
-                    $cpg = ceil($cdata/$limit);
-
-                    echo '<!-- Pagination START -->
-                          <ul class="pagination">';
-
-                    if($cdata > $limit){
-
-                        if($pg > 1){
-                            $prev = $pg - 1;
-                            echo '<li><a href="?page=sett&sub=usr&pg=1"><i class="material-icons md-48">first_page</i></a></li>
-                                  <li><a href="?page=sett&sub=usr&pg='.$prev.'"><i class="material-icons md-48">chevron_left</i></a></li>';
-                        } else {
-                            echo '<li class="disabled"><a href=""><i class="material-icons md-48">first_page</i></a></li>
-                                  <li class="disabled"><a href=""><i class="material-icons md-48">chevron_left</i></a></li>';
-                        }
-
-                        //perulangan pagging
-                        for($i=1; $i <= $cpg; $i++)
-                            if($i != $pg){
-                                echo '<li class="waves-effect waves-dark"><a href="?page=sett&sub=usr&pg='.$i.'"> '.$i.' </a></li>';
-                            } else {
-                                echo '<li class="active waves-effect waves-dark"><a href="?page=sett&sub=usr&pg='.$i.'"> '.$i.' </a></li>';
-                            }
-
-                        //last and next pagging
-                        if($pg < $cpg){
-                            $next = $pg + 1;
-                            echo '<li><a href="?page=sett&sub=usr&pg='.$next.'"><i class="material-icons md-48">chevron_right</i></a></li>
-                                  <li><a href="?page=sett&sub=usr&pg='.$cpg.'"><i class="material-icons md-48">last_page</i></a></li>';
-                        } else {
-                            echo '<li class="disabled"><a href=""><i class="material-icons md-48">chevron_right</i></a></li>
-                                  <li class="disabled"><a href=""><i class="material-icons md-48">last_page</i></a></li>';
-                        }
-                            echo ' </ul>
-                                   <!-- Pagination END -->';
-                    } else {
-                        echo '';
-                    }
                 }
             }
+        }
 ?>
